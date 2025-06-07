92bc9865dcee7a27eda02729cfefecf598d60367
erickha16 <errickha@gmail.com>
2025-06-06 18:36:03 -0600

    ApplicationDbContext created, connection string added, Program.cs updated

---
+++ MovieTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieTracker.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        //Definir los modelos para que se creen en la base de datos
+    }
+}
--- MovieTracker/Program.cs
+++ MovieTracker/Program.cs
@@ -1,4 +1,34 @@
+//Dar de alta servicios y configurar la aplicación ASP.NET Core MVC
+using Microsoft.EntityFrameworkCore;
+using MovieTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Se utiliza un operador de coalesencia, en caso de que no se encuentre la cadena de conexión, se lanza una excepción.
+var connection = builder.Configuration.GetConnectionString("DbConnection") ?? 
+    throw new InvalidOperationException("Connection string 'DbConnection' not found.");
+
+// Configurar el contexto de la base de datos con Entity Framework Core y especificar el proveedor de base de datos SQL Server.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connection));
+
+//------------------------------ Agregar Servicios personalizados ------------------------------\\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
